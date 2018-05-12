8363d2848fd19b0fe13cf9d9df557b20c51eb344
Asterios Network <36882320+vilitin@users.noreply.github.com>
2018-05-13 00:50:45 +0300

    Update constants.py
    f

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 import os
 
 __version__ = "1.2.0"
-DATMUSIC_API_ENDPOINT = os.getenv("DATMUSIC_API_ENDPOINT", "https://datmusic-api.dev/search/")
+DATMUSIC_API_ENDPOINT = os.getenv("DATMUSIC_API_ENDPOINT", "https://api-2.datmusic.xyz/search/")
 INLINE_QUERY_CACHE_TIME = 12 * 30 * 24 * 60 * 60; # 1 year
