472dfb7b9ce45f1d7d3f1a42375347f6b04bea36
Asterios Network <36882320+vilitin@users.noreply.github.com>
2018-06-14 23:11:19 +0300

    ytb.py

---
+++ ytb.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import time
+import socket
+
+os.system("title YouTube View Bot")
+os.system("color f0")
+os.system("cls")
+print("Please paste a YouTube video URL.")
+print("")
+url = input("[YouTubeViewBot url]# ")
+
+def openURL():
+	os.system("start " + url)
+	time.sleep(5)
+	openURL()
+	
+openURL()
